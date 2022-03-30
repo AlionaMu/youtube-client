75886c3f9a53f37858256b7b26c4815dff715529
AlionaMu <udova2000@mail.ru>
2022-03-30 20:23:14 +0300

    feat: add basic components

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { SearchComponent } from './search/search.component';
+import { SortingComponent } from './sorting/sorting.component';
+import { ResultsComponent } from './results/results.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header,
+    AuthComponent,
+    SearchComponent,
+    SortingComponent,
+    ResultsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
