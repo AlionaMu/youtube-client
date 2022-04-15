dbd04f057ee0985e342ff35085ffabdaf86aee2f
AlionaMu <udova2000@mail.ru>
2022-04-15 16:46:58 +0300

    feat: implement login page component view

--- youtube-client/src/app/auth/auth.module.ts
+++ youtube-client/src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
